96ba77d498b549e8f5268c143460afa5394d10f1
Grace Ryoo <ger85847@uga.edu>
2024-07-09 21:49:58 -0400

    head.c 1

--- head.c
+++ head.c
@@ -16,8 +16,24 @@
 
 
 
-int main() 
+int main(int argc, char *argv[]) 
 {
+	int opt;
+	int nflag = 0;
+	int cflag = 0;
+	int num = 10;
+	FILE *file;
+
+	while((opt = getopt(argc, argv, "c:n:")) != -1) {
+		switch (opt) {
+			case 'c':
+				cflag = 1;
+				break;
+			case 'n':
+				nflag = 1;
+				break;
+		} // switch
+	} // while
 
 
 
