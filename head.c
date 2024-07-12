15edd2a97d3da92b5213383c7392f09a85cd4d2c
Grace Ryoo <ger85847@uga.edu>
2024-07-12 19:15:10 -0400

    head.c 3

--- head.c
+++ head.c
@@ -12,6 +12,8 @@
  #include <unistd.h>
  #include <errno.h>
 
+ #define BUFF 1048576 // buffer size 
+
 
 
 
@@ -22,8 +24,8 @@ int main(int argc, char *argv[])
 	int nflag = 0;
 	int cflag = 0;
 	int num = 10;
-	int counter = 0;
-	FILE *file;
+	int fd;
+
 
 	while((opt = getopt(argc, argv, "c:n:")) != -1)
        	{
@@ -32,36 +34,53 @@ int main(int argc, char *argv[])
 			case 'c':
 				cflag = 1;
 				num = atoi(optarg);
+				if (num <= 0) {
+					fprintf(stderr, "Error: number must be a positive integer.\n");
+					exit(EXIT_FAILURE);
+				} // if
 				break;
 			case 'n':
 				nflag = 1;
 				num = atoi(optarg);
+				if (num <= 0) {
+					fprintf(stderr, "Error: number must be a positive integer.\n");
+					exit(EXIT_FAILURE);
+				} // if
 				break;
 		} // switch
 	} // while
 	
-	if (cflag) {
-	
-	
-	} else if (nflag) {
-
-	} else {
-
+	if (cflag && nflag) {
+		fprintf(stderr, "Error: cannot combine -c and -n options.");
+		exit(EXIT_FAILURE);
 	} // if
 
-	/**
-	for(; optind < argc; optind++){
+	if (optind == argc) {
+		if (cflag) {
+			c_bytes(STDIN_FILENO, num);
+		} else if (nflag) {
+			n_lines(STDIN_FILENO, num);
+		} // if
+	} else {
+		for(; optind < argc; optind++){
+			if (strcmp(argv[optind], "-") == 0) {
+				fd = STDIN_FILENO;
+			} else {
+				fd = open(argv[optind], O_RDONLY);
+				if (fd == -1) {
+					fprintf(stderr, "cannot open file %s: %s\n", argv[optind], strerror(errno));
+					continue;
+				} // if
+			} // if
+		} // for
 		
-	} // for
-*/
+		if (cflag) {
+			c_bytes(fd, num);
+		} else if (nflag) {
+			n_lines(fd, num);
+		} // if
+
 	return 0;
-
-
-
-
-
-
-
 
 
 } // main
