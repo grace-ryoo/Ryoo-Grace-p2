44de69acaac88da4d23c3dea5e4dd81196502bd2
Grace Ryoo <ger85847@uga.edu>
2024-07-11 22:19:50 -0400

    head.c 2

--- head.c
+++ head.c
@@ -10,7 +10,7 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <unistd.h>
-
+ #include <errno.h>
 
 
 
@@ -22,18 +22,42 @@ int main(int argc, char *argv[])
 	int nflag = 0;
 	int cflag = 0;
 	int num = 10;
+	int counter = 0;
 	FILE *file;
 
-	while((opt = getopt(argc, argv, "c:n:")) != -1) {
-		switch (opt) {
+	while((opt = getopt(argc, argv, "c:n:")) != -1)
+       	{
+		switch (opt) 
+		{
 			case 'c':
 				cflag = 1;
+				num = atoi(optarg);
 				break;
 			case 'n':
 				nflag = 1;
+				num = atoi(optarg);
 				break;
 		} // switch
 	} // while
+	
+	if (cflag) {
+	
+	
+	} else if (nflag) {
+
+	} else {
+
+	} // if
+
+	/**
+	for(; optind < argc; optind++){
+		
+	} // for
+*/
+	return 0;
+
+
+
 
 
 
