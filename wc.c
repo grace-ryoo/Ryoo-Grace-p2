2fd7856b592c95817d331ca2973cecbf4bd732b4
Grace Ryoo <ger85847@uga.edu>
2024-07-18 10:33:23 -0400

    wc.c 3

--- wc.c
+++ wc.c
@@ -17,6 +17,10 @@
 
 #define BUFF 1048576 // buffer size
 
+/**
+ *
+ *
+ */
 void bytes_size(char *file) 
 {
 	int fd = open(file, O_RDONLY);
@@ -37,16 +41,52 @@ void bytes_size(char *file)
 
 } // bytes_size
 
-void new_lines_size() 
+/**
+ *
+ *
+ */
+void new_lines_size(char *file) 
 {
+	int fd = open(file, O_RDONLY);
+	if (fd == -1) {
+		perror("open");
+		return;	
+	} // if
+	
+	int counter = 0;
+	int n;
+	char buffer[BUFF];
 
+	while((n = read(fd, buffer, BUFF)) > 0) {
+		for (int i = 0; i < n; i++) {
+			if (buffer[i] == '\n') {
+				counter++;
+			} // if
+		} // for
+	} // while
+	
+	if (n == -1) {
+		perror("read");
+	} else {
+		printf("The number of newlines in %s is %d\n", file, counter);
+	} // if
+
+	close(fd);
 } // new_lines_size
 
-void words_size() 
+/**
+ *
+ *
+void words_size(char *file) 
 {
+	int fd = open(file, O_RDONLY);
+	if (fd == -1) {
+		perror("open");
+		return;	
+	} // if
 
 } // words_size
-
+*/
 
 /**
  *
@@ -81,6 +121,12 @@ int main(int argc, char *argv[])
 	if (cflag + lflag + wflag == 0) { // standard input assumed
 		cflag = lflag = wflag = 1;
 	} // if
+
+	if (optind >= argc) {
+		fprintf(stderr, "missing file names\n");
+		exit(EXIT_FAILURE);
+	} // if
+	
 	for (int i = optind; i < argc; i++) {
 		char *file = argv[i];
 		
@@ -91,10 +137,11 @@ int main(int argc, char *argv[])
 		if (lflag) { // number of newlines
 			new_lines_size(file);
 		} // if
-
+/**
 		if (wflag) { // number of words
 			words_size(file);
 		} // if
+		  */
 	} // for
 
 
